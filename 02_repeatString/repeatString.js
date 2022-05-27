ab4b241b79e2ac941056b02d418429750e04667e
Samuel Paget <sjapaget@gmail.com>
2022-05-27 10:00:21 +0200

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string = '', num = Math.floor(Math.random * 1000)) {
 
+    //Check arguments received are as expected:
+    if (num < 0) {return 'ERROR'}
+
+
+    let result = '';
+
+    for (let i = 0; i < num; i++){
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
