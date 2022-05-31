e3fd6b393285b80f24d0fc391f541efc082b6b12
Samuel Paget <sjapaget@gmail.com>
2022-05-31 12:56:35 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  arr.map(item => total += item);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const arr = [1];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i)
+  }	
+  return multiply(arr)  
 };
 
 // Do not edit below this line
